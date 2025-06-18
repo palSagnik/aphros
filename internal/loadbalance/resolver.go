485ec35216db32eb462aa1aa3817682bb92f1f20
Sagnik <palsagnik2102@gmail.com>
2025-06-18 13:44:43 +0530

    resolver struct
    Signed-off-by: Sagnik <palsagnik2102@gmail.com>

--- internal/loadbalance/resolver.go
+++ internal/loadbalance/resolver.go
@@ -4,8 +4,17 @@ import (
 	"sync"
 
 	api "github.com/palSagnik/aphros/api/v1"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/serviceconfig"
 )
 
 type Resolver struct {
 	mu sync.Mutex
+	clientConn resolver.ClientConn
+	resolver *grpc.ClientConn
+	serviceConfig *serviceconfig.ParseResult
+	logger *zap.Logger
 }
+
